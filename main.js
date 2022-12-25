4a06c313e3c63e64c382cdc78abb4403db0a9182
Sarang Chaudhary <schaudhary@aiopsgroup.com>
2022-12-25 10:49:49 +0530

    chapter-1 practice

---
+++ main.js
@@ -0,0 +1,9 @@
+const header1 = React.createElement("h1", {}, "Header 1");
+const header2 = React.createElement("h1", {}, "Header 2");
+const header3 = React.createElement("h1", {}, "Header 3");
+
+const container = React.createElement("div", { className: "container" }, [header1, header2, header3])
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
