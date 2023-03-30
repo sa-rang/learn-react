671d115523026ea789556b2715aa977a7bc72d7d
Sarang Chaudhary <schaudhary@aiopsgroup.com>
2023-03-30 13:12:14 +0530

    add link

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const header1 = React.createElement("h1", {}, "Header 1");
 const header2 = React.createElement("h1", {}, "Header 2");
 const header3 = React.createElement("h1", {}, "Header 3");
+const link = React.createElement("a", { href: "https://www.bubu.com" }, "Header 3");
 
-const container = React.createElement("div", { className: "container" }, [header1, header2, header3])
+const container = React.createElement("div", { className: "container" }, [header1, header2, header3, link])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
